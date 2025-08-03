a5e7cd81e82b4f879d4f51358b5cc886ff848375
Xkonti <xkonti@xkonti.tech>
2025-08-03 13:21:25 -0400

    Expand dart sample to test the built lib

--- sample/dart/lib/dart.dart
+++ sample/dart/lib/dart.dart
@@ -1,5 +1,19 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:ffi/ffi.dart';
 import 'package:sqliteraw.dart/sqliteraw.dart';
 
 String version() {
   return SQLITE_VERSION;
+}
+
+String versionFromLibrary() {
+  final library = Platform.isLinux
+      ? DynamicLibrary.open('assets/libsqlite3.so')
+      : Platform.isWindows
+          ? DynamicLibrary.open('assets/sqlite3.dll')
+          : DynamicLibrary.open('assets/libsqlite3.dylib');
+  
+  final sqlite = NativeLibrary(library);
+  return sqlite.sqlite3_libversion().cast<Utf8>().toDartString();
 }
--- sample/dart/test/dart_test.dart
+++ sample/dart/test/dart_test.dart
@@ -1,3 +1,25 @@
 
+import 'package:dart/dart.dart' as dart;
+import 'package:test/test.dart';
+
 void main() {
+  group('SQLite Version Tests', () {
+    test('version() returns valid compile-time SQLite version', () {
+      final version = dart.version();
+      expect(version, isNotEmpty);
+      expect(version, matches(RegExp(r'^\d+\.\d+\.\d+')));
+    });
+
+    test('versionFromLibrary() returns valid runtime SQLite version via FFI', () {
+      final version = dart.versionFromLibrary();
+      expect(version, isNotEmpty);
+      expect(version, matches(RegExp(r'^\d+\.\d+\.\d+')));
+    });
+
+    test('both version functions return the same value', () {
+      final compileTimeVersion = dart.version();
+      final runtimeVersion = dart.versionFromLibrary();
+      expect(compileTimeVersion, equals(runtimeVersion));
+    });
+  });
 }
