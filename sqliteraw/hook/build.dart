ceb273bc6c8cdb35d5413f19414d6621a4fa06fb
Dharmik <dharmik.azzure@gmail.com>
2026-06-26 21:07:15 +0530

    Add Dart Native Assets build hook for SQLite

---
+++ sqliteraw/hook/build.dart
@@ -0,0 +1,21 @@
+import 'package:hooks/hooks.dart';
+import 'package:native_toolchain_c/native_toolchain_c.dart';
+
+void main(List<String> args) async {
+  await build(args, (input, output) async {
+    final cBuilder = CBuilder.library(
+      name: 'sqliteraw',
+      assetName: 'sqliteraw.dart',
+      sources: [
+        '../sqlite/sqlite3.c',
+      ],
+      defines: {
+        'SQLITE_ENABLE_FTS4': null,
+        'SQLITE_ENABLE_FTS5': null,
+        'SQLITE_ENABLE_RTREE': null,
+        'SQLITE_ENABLE_GEOPOLY': null,
+      },
+    );
+    await cBuilder.run(input: input, output: output);
+  });
+}
