5e7a3a88a12f9b34a3373816a535f87f9527aa79
Taras Mazepa <git@okl.pw>
2026-06-27 18:56:42 -0700

    Minor updates

--- sample/dart/test/dart_test.dart
+++ sample/dart/test/dart_test.dart
@@ -1,12 +1,14 @@
 import 'package:ffi/ffi.dart';
-import 'package:sqliteraw.dart/sqliteraw.dart' as ffi;
+import 'package:sqliteraw.dart/sqliteraw.dart' as sqliteraw;
 import 'package:test/test.dart';
 
 void main() {
   test('sqlite3_libversion', () {
-    final versionPtr = ffi.sqlite3_libversion();
-    final versionStr = versionPtr.cast<Utf8>().toDartString();
+    final versionStr = sqliteraw
+        .sqlite3_libversion()
+        .cast<Utf8>()
+        .toDartString();
     expect(versionStr, isNotEmpty);
-    expect(versionStr, ffi.SQLITE_VERSION);
+    expect(versionStr, sqliteraw.SQLITE_VERSION);
   });
 }
--- sqliteraw/hook/build.dart
+++ sqliteraw/hook/build.dart
@@ -3,19 +3,16 @@ import 'package:native_toolchain_c/native_toolchain_c.dart';
 
 void main(List<String> args) async {
   await build(args, (input, output) async {
-    final cBuilder = CBuilder.library(
+    await CBuilder.library(
       name: 'sqliteraw',
       assetName: 'sqliteraw.dart',
-      sources: [
-        '../sqlite/sqlite3.c',
-      ],
+      sources: ['../sqlite/sqlite3.c'],
       defines: {
         'SQLITE_ENABLE_FTS4': null,
         'SQLITE_ENABLE_FTS5': null,
         'SQLITE_ENABLE_RTREE': null,
         'SQLITE_ENABLE_GEOPOLY': null,
       },
-    );
-    await cBuilder.run(input: input, output: output);
+    ).run(input: input, output: output);
   });
 }
