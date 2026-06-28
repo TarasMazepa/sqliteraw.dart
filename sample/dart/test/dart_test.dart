90d6f81390b6e1ba6c500486ea5c0f56c629f81b
ghoridharmik <124550932+ghoridharmik@users.noreply.github.com>
2026-06-28 07:29:42 +0530

    Add Dart Native Assets build hook for SQLite (#69)
    * Add Dart Native Assets build hook for SQLite
    
    * Minor updates
    
    * Minor updates
    
    ---------
    
    Co-authored-by: Taras Mazepa <git@okl.pw>

--- sample/dart/test/dart_test.dart
+++ sample/dart/test/dart_test.dart
@@ -1,3 +1,12 @@
+import 'package:ffi/ffi.dart';
+import 'package:sqliteraw.dart/sqliteraw.dart' as sqliteraw;
+import 'package:test/test.dart';
 
 void main() {
+  test('sqlite3_libversion', () {
+    expect(
+      sqliteraw.sqlite3_libversion().cast<Utf8>().toDartString(),
+      sqliteraw.SQLITE_VERSION,
+    );
+  });
 }
