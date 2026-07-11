6851d14289db03b3cb6e694e36840ad85aaa385f
Dharmik <dharmik.azzure@gmail.com>
2026-07-11 13:41:30 +0530

    Split native FFI bindings into separate module

---
+++ sqliteraw/lib/src/web/bindings_stub.dart
@@ -0,0 +1,7 @@
+// Web stub for the native ffi bindings. Use sqliteraw_web.dart for wasm access.
+// ignore_for_file: constant_identifier_names
+library;
+
+const String SQLITE_VERSION = '3.53.3';
+
+const int SQLITE_VERSION_NUMBER = 3053003;
