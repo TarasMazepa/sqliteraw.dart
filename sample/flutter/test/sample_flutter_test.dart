95e061f9bdb6fdf44c8ec815273f16c78e0a75f8
Taras Mazepa <6552358+TarasMazepa@users.noreply.github.com>
2026-07-15 06:52:41 -0700

    Update sample_flutter_test.dart

--- sample/flutter/test/sample_flutter_test.dart
+++ sample/flutter/test/sample_flutter_test.dart
@@ -1,12 +1,9 @@
 import 'package:ffi/ffi.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sqliteraw/sqliteraw_native.dart' as ffi;
+import 'package:sqliteraw/sqliteraw_native.dart';
 
 void main() {
   test('sqlite3_libversion', () {
-    final versionPtr = ffi.sqlite3_libversion();
-    final versionStr = versionPtr.cast<Utf8>().toDartString();
-    expect(versionStr, isNotEmpty);
-    expect(versionStr, ffi.SQLITE_VERSION);
+    expect(sqlite3_libversion().cast<Utf8>().toDartString(), SQLITE_VERSION);
   });
 }
